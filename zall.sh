e04e4e43170dd5b621afc6b72826c01f17c7661f
Islamic Cyber Team <radengozal@gmail.com>
2019-04-08 20:33:16 +0700

    Add files via upload

---
+++ zall.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#!/gue/gan
+clear
+echo "wait for a few second"
+sleep 3
+
+figlet FBHACK | lolcat
+sleep 1
+echo "################################" | lolcat
+echo "#    Author : LEADIS BOOHAY    #" | lolcat
+echo "#    code   : Bash shell       #" | lolcat
+echo "################################" | lolcat
+sleep 2
+
+read -p "Masukan USERNAME / ID / EMAIL  :" target;
+sleep 2
+echo "<---------LOGGED TO  FB-------->" 
+sleep 3
+echo "$target Vuln........." | lolcat
+sleep 5
+echo "Apakah target $target";
+read -p "y: " ny;
+echo "Prosess..........."
+echo "ATTACKED to $target";
+echo "ATTACKED to $target";
+echo "ATTACKED to $target";
+echo "ATTACKED to $target";
+echo "ATTACKED to $target";
+echo "---------------------------" | lolcat
+echo "Username : Trii Rahma"
+echo "Account Date : Juni 2014"
+echo "City : Karang Anyar , Indonesia"
+echo "Follower : 2313 people"
+echo "Proxy : 103.107.133.17:33236"
+echo "Last Login : Today" 
+echo "Password : fbrahma@@"
+echo "---------------------------" | lolcat
+read -p "Cari Lagi [Y/N]" ya;
